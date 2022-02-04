2486ab59da55dfb2e9c076db66d45dd913bed1f5
Kashif Naeem <kashifnaeem228@gmail.com>
2022-02-04 10:44:39 +0500

    adding servers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'components-databinding';
+  serverElements: any[] = [];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    })
+  }
+  
 }
